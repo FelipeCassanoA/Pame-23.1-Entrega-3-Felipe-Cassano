dd415c624b7744dcf77b0ef9dd176831a6ffebba
FelipeCassanoA <felipe.cassano@gmail.com>
2023-07-12 13:03:57 -0300

    commit4

--- ever_mart/src/components/Navbar.js
+++ ever_mart/src/components/Navbar.js
@@ -1,24 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faHeart } from '@fortawesome/free-solid-svg-icons';
 import React from "react";
 import Logo from "../img/imageLogo.png";
-import Barra from "../img/Barrapesquisa.png"
-import "./Navbar.css"
+import Barra from "../img/Barrapesquisa.png";
+import "./Navbar.css";
+
 
 function Navbar() {
     return (
     <div className = "navbar">
         
-        <div className ="leftside">
-            <img src={Logo} alt= ""/>    
-            <h1>TendTudo</h1>            
+        <div className ="leftSide">
+            <img src={Logo} alt=""/> 
+            <p className="title">TendTudo </p>   
         </div>
         
-        <div className = "rightside">   </div>
+        <div className = "rightSide">   
+            <button> Login </button>
+            <div className = "Separar"> </div>
+            <button> Cadastrar</button>
+        </div> 
+        
+        <div className='adicional'>
+            <p><FontAwesomeIcon icon={faShoppingCart} /> Carrinho</p>
+            <p><FontAwesomeIcon icon={faHeart} /> Seleção</p>        
+        </div>
 
-        <div className ="barra">   </div>
-            <input type="text" placeholder="Search">
-            
+        <div className ="barra">  
+            <input type="text" placeholder="Search" id="procurar" >
             </input>
-            <img src= {Barra} alt=""/>
+            <img src= {Barra} alt="" id="lupa"/>
+         </div>
 
     </div>
   
